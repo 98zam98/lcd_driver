47a0b8dc773b40ee8c824c26da49e4413437fa1f
zyad <zyadmackawy@gmail.com>
2021-11-18 12:51:50 -0500

    zzz

--- lcd.h
+++ lcd.h
@@ -3,5 +3,90 @@
 
 #include "hardware.h"
 
+// commands
+
+#define clear_display_screen 0x01
+
+#define return_home 0x02
+
+#define shift_cursor_to_left 0x04
+
+#define shift_cursor_to_right 0x06
+
+#define shift_display_left 0x07
+
+#define shift_display_right 0x05
+
+// =======================================
+
+#define clear_display_screen
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
+
+#define clear_display_screen 1
 
 #endif /* LCD_H_ */
--- main.c
+++ main.c
@@ -4,7 +4,19 @@
 #include "macros.h"
 #include "hardware.h"
 
+#define LCD_DPRT lcd_data_PORT
 
+#define LCD_DDDR lcd_data_dir
+
+#define LCD_CPRT lcd_com_PORT
+
+#define LCD_CDDR lcd_com_dir
+
+#define LCD_RS RS_lcd_com_digit
+
+#define LCD_RW RW_lcd_com_digit
+
+#define LCD_E E_lcd_com_digit
 
 
 int main() {	
