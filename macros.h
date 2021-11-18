663261da5b32616026b156e69ed871e4894edfc2
zyad <zyadmackawy@gmail.com>
2021-11-18 14:00:05 -0500

    zaz

--- macros.h
+++ macros.h
@@ -9,6 +9,12 @@
 #define checkbit(reg,shift) ((reg) & (1<<(shift)))
 #define checkbit0(reg,shift) (((reg) & (1<<(shift)))>>shift)
 #define assignbit(reg,shift,val) reg|=(val<<shift)
+
+#define send_high_nibble(reg,val) reg =(val&0xf0)
+
+#define clear4bit(reg,shift) reg&=~(0x0f<<shift)
+
+#define send_high_nibble_to_shift(reg,shift,val) reg |=(val&0xf0)>>(4-shift)
 // macros end
 
 #endif
--- main.c
+++ main.c
@@ -19,6 +19,88 @@
 #define LCD_E E_lcd_com_digit
 
 
+#define lcd_send_4bit(val) high_nibble_to_shift(lcd_data_PORT,lcd_data_digit,val)
+
+
+
+#define rs_com() clearbit(lcd_com_PORT,RS_lcd_com_digit)
+
+#define rs_data() setbit(lcd_com_PORT,RS_lcd_com_digit)
+
+#define rw_write() clearbit(lcd_com_PORT,RW_lcd_com_digit)
+
+  
+void E_h2l()
+{
+  setbit(lcd_com_PORT,E_lcd_com_digit);
+  _delay_us(1);
+  clearbit(lcd_com_PORT,E_lcd_com_digit);
+  _delay_us(100);
+}
+
+void lcdCommand(unsigned char cmnd)
+{
+  //lcd_data_PORT =(cmnd&0xf0)>>(4-lcd_data_digit);
+  clear4bit(lcd_data_PORT,lcd_data_digit);
+  lcd_send_4bit(cmnd);
+  //clearbit(lcd_com_PORT,RS_lcd_com_digit);
+  rs_com();
+  //clearbit(lcd_com_PORT,RW_lcd_com_digit);
+  rw_write();
+  
+  E_h2l();
+  
+  //lcd_data_PORT =((cmnd<<4)&0xf0)>>(4-lcd_data_digit);
+  clear4bit(lcd_data_PORT,lcd_data_digit);
+  lcd_send_4bit((cmnd<<4));
+
+  E_h2l();
+}
+
+
+void lcdData(unsigned char data)
+{
+  clear4bit(lcd_data_PORT,lcd_data_digit);
+  lcd_send_4bit(data);
+  //clearbit(lcd_com_PORT,RS_lcd_com_digit);
+  rs_data();
+  //clearbit(lcd_com_PORT,RW_lcd_com_digit);
+  rw_write();
+  
+  E_h2l();
+  
+  //lcd_data_PORT =((cmnd<<4)&0xf0)>>(4-lcd_data_digit);
+  clear4bit(lcd_data_PORT,lcd_data_digit);
+  lcd_send_4bit((data<<4));
+
+  E_h2l();
+}
+
+void lcd_init()
+{
+  high_nibble_to_shift(lcd_data_dir,lcd_data_digit,0xff);
+  
+  setbit(lcd_com_dir,RS_lcd_com_digit);
+  setbit(lcd_com_dir,RW_lcd_com_digit);
+  setbit(lcd_com_dir,E_lcd_com_digit);
+  
+  clearbit(lcd_com_PORT,E_lcd_com_digit);
+
+  lcdCommand(0x33);
+  lcdCommand(0x32);
+  lcdCommand(0x28);
+  lcdCommand(0x0e);
+  lcdCommand(0x01);
+
+  _delay_us(2000);
+  
+  lcdCommand(0x06);
+}
+
+void lcd_gotoxy(unsigned char x,unsigned char y)
+{}
+
+
 int main() {	
 	while(1)
   {
